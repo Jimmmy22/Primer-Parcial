6d84fa940c4d385fa497979ca1ce9b02e62c12e2
Jimmmy22 <jorz22.service@gmail.com>
2022-09-22 18:48:22 -0400

    Se agrega el Proyecto

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { BodyComponent } from './body/body.component';
+import { BarraLateralComponent } from './barra-lateral/barra-lateral.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    BodyComponent,
+    BarraLateralComponent,
+    FooterComponent,
+],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
